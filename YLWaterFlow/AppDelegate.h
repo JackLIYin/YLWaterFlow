ea16b59ca0b0176f325ef59bc36342adf834b6ae
LeonYing <18010476951@189.cn>
2016-01-13 20:01:05 +0800

    Initial Commit

---
+++ YLWaterFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLWaterFlow
+//
+//  Created by NaiveVDisk on 16/1/13.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
